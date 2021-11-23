8d883e6cbd2045866dd1413201322de3b6761cba
17020940 <ducnha99bn@gmail.com>
2021-11-23 22:59:49 +0700

    chuyen model sang pointer

--- controller/user/user-controller.go
+++ controller/user/user-controller.go
@@ -2,23 +2,25 @@ package userController
 
 import (
 	"go-gin/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func CreateUser(c *gin.Context) {
-	var user models.User
-	c.ShouldBindJSON(&user)
-	hashPassword, _ := hashPassword(user.Password)
-	user.Password = hashPassword
-	user.Email = "test"
-	user.PhoneNumber = "test"
-	models.DB.Create(&user)
+	var user = &models.User{}
+	if err := c.ShouldBindJSON(user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.Password, _ = hashPassword(user.Password)
+	models.DB.Create(user)
 	c.JSON(200, user)
 }
 
-func hashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(bytes), err
+func hashPassword(password *string) (*string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(*password), bcrypt.DefaultCost)
+	hashPassword := string(bytes)
+	return &hashPassword, err
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	ID          uint `gorm:"primaryKey"`
-	Username    string
-	Password    string
-	PhoneNumber string
-	Email       string
+	ID          *uint   `json:"id" gorm:"primaryKey"`
+	Username    *string `json:"username" binding:"required"`
+	Password    *string `json:"password" binding:"required"`
+	PhoneNumber *string `json:"phoneNumber" binding:"required"`
+	Email       *string `json:"email" binding:"required,email"`
 }
